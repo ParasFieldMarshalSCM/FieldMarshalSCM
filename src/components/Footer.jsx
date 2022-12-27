4408bd312c7300083f52f46b49579ef5d9971efd
Abhishek Shaji <abhishek@Abhisheks-MacBook-Pro.local>
2022-12-27 20:46:52 +0100

    update links and header design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -153,17 +153,17 @@ const Footer = () => (
                         <li>
                             <a
                                 className="text-teal-800 hover:text-slate-300 transition duration-150 ease-in-out"
-                                href="src/partials#0"
+                                href="https://calendly.com/parasgoel_vf"
                             >
-                                Our Story
+                                Book a Demo
                             </a>
                         </li>
                         <li>
                             <a
                                 className="text-teal-800 hover:text-slate-300 transition duration-150 ease-in-out"
-                                href="src/partials#0"
+                                href="https://www.linkedin.com/company/fieldmarshalscm"
                             >
-                                Work With Us
+                                LinkedIn
                             </a>
                         </li>
                     </ul>
@@ -171,54 +171,6 @@ const Footer = () => (
             </div>
             {/* Bottom area */}
             <div className="md:flex md:items-center md:justify-between pb-4 md:pb-8">
-                {/* Social links */}
-                <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
-                    <li>
-                        <a
-                            className="flex justify-center items-center text-emerald-600 hover:text-slate-300 transition duration-150 ease-in-out"
-                            href="src/partials#0"
-                            aria-label="Twitter"
-                        >
-                            <svg
-                                className="w-8 h-8 fill-current"
-                                viewBox="0 0 32 32"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z" />
-                            </svg>
-                        </a>
-                    </li>
-                    <li className="ml-2">
-                        <a
-                            className="flex justify-center items-center text-emerald-600 hover:text-slate-300 transition duration-150 ease-in-out"
-                            href="src/partials#0"
-                            aria-label="Medium"
-                        >
-                            <svg
-                                className="w-8 h-8 fill-current"
-                                viewBox="0 0 32 32"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path d="M23 8H9a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1Zm-1.708 3.791-.858.823a.251.251 0 0 0-.1.241V18.9a.251.251 0 0 0 .1.241l.838.823v.181h-4.215v-.181l.868-.843c.085-.085.085-.11.085-.241v-4.887l-2.41 6.131h-.329l-2.81-6.13V18.1a.567.567 0 0 0 .156.472l1.129 1.37v.181h-3.2v-.181l1.129-1.37a.547.547 0 0 0 .146-.472v-4.749a.416.416 0 0 0-.138-.351l-1-1.209v-.181H13.8l2.4 5.283 2.122-5.283h2.971l-.001.181Z" />
-                            </svg>
-                        </a>
-                    </li>
-                    <li className="ml-2">
-                        <a
-                            className="flex justify-center items-center text-emerald-600 hover:text-slate-300 transition duration-150 ease-in-out"
-                            href="src/partials#0"
-                            aria-label="Github"
-                        >
-                            <svg
-                                className="w-8 h-8 fill-current"
-                                viewBox="0 0 32 32"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
-                            </svg>
-                        </a>
-                    </li>
-                </ul>
                 {/* Links */}
                 <div className="text-sm text-slate-600">
                     <a
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,29 +5,31 @@ import Logo from '../images/logo-v2.png'
 
 const Header = () => (
     <header className="absolute w-full z-30">
-        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="px-4 sm:px-6">
             <div className="flex items-center justify-between h-16 md:h-20">
                 {/* Site branding */}
                 <div className="shrink-0 mr-4">
                     {/* Logo */}
-                    <Link className="block" to="/" aria-label="Cruip">
-                        <img className="w-12 h-12" src={Logo} alt="logo" />
+                    <Link className="flex items-center" to="/" aria-label="Cruip">
+                        <img className="w-12 h-12" src={Logo} alt="logo" /> <span className="font-bold ml-2">FieldMarshalSCM</span>
                     </Link>
                 </div>
-                {/* Desktop navigation */}
-                {/*<nav className="flex grow">*/}
-                {/*  /!* Desktop sign in links *!/*/}
-                {/*  <ul className="flex grow justify-end flex-wrap items-center">*/}
-                {/*    <li>*/}
-                {/*      <Link className="font-medium text-teal-800 hover:text-slate-300 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out" to="/signin">Sign in</Link>*/}
-                {/*    </li>*/}
-                {/*    <li className="ml-3">*/}
-                {/*      <Link className="btn-sm text-white bg-emerald-600 hover:bg-indigo-600 w-full shadow-sm group" to="/signup">*/}
-                {/*        Get Started <span className="tracking-normal text-sky-300 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>*/}
-                {/*      </Link>*/}
-                {/*    </li>*/}
-                {/*  </ul>*/}
-                {/*</nav>*/}
+                <nav className="flex grow">
+                  {/* Desktop sign in links */}
+                  <ul className="flex grow justify-end flex-wrap items-center">
+                    <li className="hidden sm:block">
+                      <a className="font-medium text-teal-800 hover:text-slate-300 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out" href="https://www.linkedin.com/company/fieldmarshalscm/">LinkedIn</a>
+                    </li>
+                    <li className="ml-3">
+                      <a className="hidden sm:block btn-sm text-white bg-emerald-600 hover:bg-indigo-600 w-full shadow-sm group" href="https://calendly.com/parasgoel_vf">
+                        Book a demo <span className="tracking-normal text-sky-300 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
+                      </a>
+                        <a className="block sm:hidden btn-sm text-white bg-emerald-600 hover:bg-indigo-600 w-full shadow-sm group" href="https://calendly.com/parasgoel_vf">
+                            Demo <span className="tracking-normal text-sky-300 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
+                        </a>
+                    </li>
+                  </ul>
+                </nav>
             </div>
         </div>
     </header>
